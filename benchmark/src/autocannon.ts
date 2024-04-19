11b294ae1ed116fca052bedff1419991f3f78aaf
freedomers <134682608+freedomers@users.noreply.github.com>
2024-04-19 19:25:39 +0800

    Create autocannon.ts

---
+++ benchmark/src/autocannon.ts
@@ -0,0 +1,52 @@
+// Copyright IBM Corp. and LoopBack contributors 2018,2020. All Rights Reserved.
+// Node module: @loopback/benchmark
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import assert from 'assert';
+import autocannon from 'autocannon';
+
+export interface EndpointStats {
+  requestsPerSecond: number;
+  latency: number;
+}
+
+export class Autocannon {
+  constructor(
+    protected url: string,
+    protected duration: number,
+  ) {}
+
+  async execute(
+    title: string,
+    urlPath: string,
+    options: Omit<autocannon.Options, 'url'> = {},
+  ): Promise<EndpointStats> {
+    const config = {
+      url: this.buildUrl(urlPath),
+      duration: this.duration,
+      title,
+      ...options,
+      headers: {
+        'content-type': 'application/json',
+        ...options.headers,
+      },
+    };
+
+    const data = await autocannon(config);
+    assert.equal(
+      data.non2xx,
+      0,
+      'No request should have failed with non-2xx status code.',
+    );
+    const stats: EndpointStats = {
+      requestsPerSecond: data.requests.average,
+      latency: data.latency.average,
+    };
+    return stats;
+  }
+
+  protected buildUrl(urlPath: string) {
+    return this.url + urlPath;
+  }
+}
